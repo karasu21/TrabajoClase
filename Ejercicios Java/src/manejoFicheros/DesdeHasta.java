57c8897853f436d2246ba11004e41de704e50335
alumno <alumno@dam112>
2018-04-20 12:20:27 +0200

    DesdeHasta - Entrada

--- Ejercicios Java/src/manejoFicheros/DesdeHasta.java
+++ Ejercicios Java/src/manejoFicheros/DesdeHasta.java
@@ -3,7 +3,10 @@ package manejoFicheros;
 public class DesdeHasta {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Introduce numero (desde)");
+		int desde=Entrada.entero();
+		System.out.println("Introduce numero (hasta)");
+		int hasta=Entrada.entero();
 
 	}
 
