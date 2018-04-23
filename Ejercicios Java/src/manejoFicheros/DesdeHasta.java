7c02e409aa2a4bbab6f4ea42efcaa23a50afa123
Daniel <Daniel@DESKTOP-7REQ172>
2018-04-23 18:08:37 +0200

    DesdeHasta

--- Ejercicios Java/src/manejoFicheros/DesdeHasta.java
+++ Ejercicios Java/src/manejoFicheros/DesdeHasta.java
@@ -1,13 +1,29 @@
 package manejoFicheros;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class DesdeHasta {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Introduce numero (desde)");
-		int desde=Entrada.entero();
+		int desde = Entrada.entero();
 		System.out.println("Introduce numero (hasta)");
-		int hasta=Entrada.entero();
-
+		int hasta = Entrada.entero();
+		PrintWriter pw = new PrintWriter(new FileWriter(new File("E:\\PROGRAMACION\\Prueba\\desdeHasta.txt"), false));
+		DataOutputStream dos = new DataOutputStream(
+				new FileOutputStream(new File("E:\\PROGRAMACION\\Prueba\\desdeHasta.bin")));
+		for (int i = desde; i <= hasta; i++) {
+			dos.writeInt(i);
+			pw.println(i);
+		}
+		dos.close();
+		pw.close();
 	}
 
 }
