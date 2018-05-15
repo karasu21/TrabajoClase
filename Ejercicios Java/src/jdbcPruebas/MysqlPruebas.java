53b8a53f6343cdfcb42376dbc14e9c3f213be522
alumno <alumno@dam112>
2018-05-15 11:33:54 +0200

    Clase 15/05

--- Ejercicios Java/src/jdbcPruebas/MysqlPruebas.java
+++ Ejercicios Java/src/jdbcPruebas/MysqlPruebas.java
@@ -8,7 +8,7 @@ public class MysqlPruebas {
 
 	public static void main(String[] args) throws SQLException {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver");
 			Connection cn = DriverManager.getConnection("jdbc:mysql://localhost", "root", "");
 		}catch(ClassNotFoundException ex){
 			System.out.println("No se encontro el Driver MySql para JDBC.");
